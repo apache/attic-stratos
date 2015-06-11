d0c93d0002f18e544f564ac85892a48ac159cfd7
Pranavan <234rana@gmail.com>
2015-06-11 11:08:07 +0530

    Window processor added

--- extensions/cep/stratos-cep-extension/src/main/java/org/apache/stratos/cep/extension/CurveFinderWindowProcessor.java
+++ extensions/cep/stratos-cep-extension/src/main/java/org/apache/stratos/cep/extension/CurveFinderWindowProcessor.java
@@ -1,4 +1,24 @@
 package org.apache.stratos.cep.extension;
 
-public class CurveFinderWindowProcessor {
+import org.wso2.siddhi.core.persistence.ThreadBarrier;
+import org.wso2.siddhi.core.query.processor.window.RunnableWindowProcessor;
+import org.wso2.siddhi.core.query.processor.window.WindowProcessor;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+public class CurveFinderWindowProcessor extends WindowProcessor implements RunnableWindowProcessor{
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void setScheduledExecutorService(ScheduledExecutorService scheduledExecutorService) {
+
+    }
+
+    @Override
+    public void setThreadBarrier(ThreadBarrier threadBarrier) {
+
+    }
 }
