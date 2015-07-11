57efe03195b1df24e8a31cd4d0a0a78f3049e784
dk <dhananjaya92@gmail.com>
2015-07-11 13:06:17 +0530

    DataBase Connection for HealthStat

---
+++ components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/handlers/ConnectionHandler.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.stratos.rest.endpoint.handlers;
+
+import org.wso2.carbon.ndatasource.core.DataSourceManager;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectionHandler {
+    public ResultSet GetsqlConnection(String sql) {
+
+        DataSource dataSource = null;
+        String stringSql = sql;
+        ResultSet resultSet = null;
+
+        try{
+            boolean isJndiLookup = true;
+
+            if(isJndiLookup) {
+
+                // Obtain the datasource via a JNDI lookup, by passing the jndi config name
+                dataSource = (DataSource) InitialContext.doLookup("jdbc/DataSourcetoPublishHealthStatRDBMS");
+
+            } else {
+
+                // Obtain the datasource by passing the data source name
+                dataSource = (DataSource) DataSourceManager.getInstance()
+                        .getDataSourceRepository().getDataSource("DataSourcetoPublishHealthStatRDBMS")
+                        .getDSObject();
+            }
+
+
+            if (dataSource != null) {
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement();
+
+                resultSet = statement.executeQuery(stringSql);
+
+                resultSet.close();
+                statement.close();
+                connection.close();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error " + e.getMessage());
+        }
+        return resultSet;
+    }
+}
+
