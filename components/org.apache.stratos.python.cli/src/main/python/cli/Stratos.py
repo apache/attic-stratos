543198c9b03fae897015c013d5ff8c1b57634ca6
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-08-25 06:52:45 +0530

    improved formatting

--- components/org.apache.stratos.python.cli/src/main/python/cli/Stratos.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Stratos.py
@@ -395,12 +395,11 @@ class Stratos:
 
     @staticmethod
     def remove_kubernetes_cluster(kubernetes_cluster_id):
-        return Stratos.delete('kubernetesClusters/'+kubernetes_cluster_id,
-                              )
+        return Stratos.delete('kubernetesClusters/'+kubernetes_cluster_id)
+
     @staticmethod
     def remove_kubernetes_host(kubernetes_cluster_id, host_id):
-        return Stratos.delete('kubernetesClusters/'+kubernetes_cluster_id+"/hosts/"+host_id,
-                              )
+        return Stratos.delete('kubernetesClusters/'+kubernetes_cluster_id+"/hosts/"+host_id)
 
     """
     # Domain Mapping
@@ -420,8 +419,7 @@ class Stratos:
 
     @staticmethod
     def add_domain_mapping(application_id, json):
-        return Stratos.post('applications/'+application_id+'/domainMappings', json,
-                            )
+        return Stratos.post('applications/'+application_id+'/domainMappings', json)
 
     """
     # Utils
